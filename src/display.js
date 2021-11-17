9b8e35d15abb4db866b54e1a38290efd7e2a4a8b
Somdotta07 <jhilick.s@gmail.com>
2021-11-17 13:19:17 +0100

    Add api and populate DOM

--- src/display.js
+++ src/display.js
@@ -1,21 +1,30 @@
-export const displayPage = (list,ulCon, likes) => {
-    let innerCon = '';
-    list.forEach((element) => {
-        const {id,title,album} = element;
-        const likesNum = addLikes(likes,id);
-        innerCon += `
-        <li id="${id}" class="cards">
-        <div class="album-image">
-        <img src="${album}" alt="album ${title}">
-        </div>
-        <div class="likes">
-        <p data-add-like='${id}' class="likes-btn">
-        <i class="far fa-heart"></i>
-        </p>
-        <p>${likesNum} likes</p>
-        </div>
-        <p class="comment-btn">Comments</p>
-        </li>`
-    });
-    ulCon.innerHTML = innerCon;
+const addLikes = (likes,id) =>{
+    const likedAlbum = likes.find((likeAlbum) => likeAlbum.item_id === id);
+        if(!likedAlbum){
+            return 0;
+        }
+       return likedAlbum.likes;
 };
+
+
+// export const displayPage = (list,ulCon, likes) => {
+//     let innerCon = '';
+//     list.forEach((element) => {
+//         const {id,title,cover} = element;
+//         const likesNum = addLikes(likes,id);
+//         innerCon += `
+//         <li id="${id}" class="cards">
+//         <div class="album-image">
+//         <img src="${cover}" alt="album ${title}">
+//         </div>
+//         <div class="likes">
+//         <p data-add-like='${id}' class="likes-btn">
+//         <i class="far fa-heart"></i>
+//         </p>
+//         <p>${likesNum} likes</p>
+//         </div>
+//         <button class="comment-btn">Comments</button>
+//         </li>`
+//     });
+//     ulCon.innerHTML = innerCon;
+// };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,44 @@
 import './style.css';
-import { displayPage } from './display';
+// import { displayPage } from './display';
+
 const listContainer = document.getElementById('cards-c');
 
+
+
+
+
+function displayImg() {
+    fetch(`https://www.breakingbadapi.com/api/characters`)
+    .then(response => response.json())
+    .then(data => {
+        let innerCon = '';
+        if(data){
+            data.forEach(char => {
+                innerCon += `
+        <li id="${char.char_id}" class="cards">
+        <div class="album-image">
+        <img src="${char.img}" alt="album ${char.name}">
+      
+        </div>
+        <div class="likes">
+        <p class="char-name"> ${char.name}</p>
+        <p data-add-like='${char.char_id}' class="likes-btn">
+        <i class="far fa-heart">Likes 0</i>
+        </p>
+        </div>
+        <button class="comment-btn">Comments</button>
+        </li>`;
+            });
+        }else {
+            innerCon = 'Sorry';
+        }
+        listContainer.innerHTML = innerCon;
+    });
+}
+
+
+
+
+window.onload = () =>{
+    displayImg();
+}
