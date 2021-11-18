7d83d672eb1a29b6d4b7b896be9fc4720fbfc867
Somdotta07 <jhilick.s@gmail.com>
2021-11-18 01:51:17 +0100

    Add like & counter

--- src/index.js
+++ src/index.js
@@ -1,19 +1,59 @@
 import './style.css';
-// import { displayPage } from './display';
+import Storage from './storage';
 
 const listContainer = document.getElementById('cards-c');
+const baseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps';
+const appKey = 'uNvsOeXZeqiXKjfZf15O';
+
+ const getLikes = async () => {
+    const updateLike = await fetch(`${baseUrl}/${appKey}/likes/`, { method: 'GET' });
+    const likes = await updateLike.text();
+    return likes;
+  };
+  
+ const postLikes = async (charId) => {
+    await fetch(`${baseUrl}/${appKey}/likes/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        item_id: `${charId}`,
+      }),
+    });
+  };
+
+//   const likesBtn = (buttons) =>{
+//       buttons.forEach((btn)=>{
+//           btn.addEventListener('click', async()=>{
+//               console.log('Hi')
+//               await postLikes(btn.dataset.addLike);
+//               displayImg();
+//           })
+//       })
+//   }
 
 
+const addLikes= (likes, id) => {
+    const likedChar = likes.find((char)=>char.item_id===id);
+    if(!likedChar){
+        return 1;
+    }
+    return likedChar.likes;
+};
 
-
+  
 
 function displayImg() {
     fetch(`https://www.breakingbadapi.com/api/characters`)
     .then(response => response.json())
     .then(data => {
+        data = data.slice(0,6);
+        
         let innerCon = '';
         if(data){
             data.forEach(char => {
+                const likesNum = addLikes( data ,char.char_id);
                 innerCon += `
         <li id="${char.char_id}" class="cards">
         <div class="album-image">
@@ -22,9 +62,10 @@ function displayImg() {
         </div>
         <div class="likes">
         <p class="char-name"> ${char.name}</p>
-        <p data-add-like='${char.char_id}' class="likes-btn">
-        <i class="far fa-heart">Likes 0</i>
+        <p id='${char.char_id}' class="likes-btn">
+        <i class="far fa-heart"></i>
         </p>
+        <p>${likesNum} likes</p>
         </div>
         <button class="comment-btn">Comments</button>
         </li>`;
@@ -33,12 +74,30 @@ function displayImg() {
             innerCon = 'Sorry';
         }
         listContainer.innerHTML = innerCon;
-    });
+         });
 }
+// Item Counter
+const itemCounter = () => {
+    const cards = document.querySelectorAll('.cards');
+        return cards.length;
+};
 
-
+const displayCount = () =>{
+    let option = document.querySelector('.nav-list-item.all');
+    const active = Storage.getItem('active');
+    if(active) {
+        option = document.querySelector(`nav-list-item.${active}`);
+        Storage.setItem('active','');
+    }
+    const span =document.createElement('span');
+    span.className = 'item-counter';
+    span.innerHTML += `(${6})`;// Need to check
+    option.style.color = '#000';
+    option.appendChild(span);
+};
 
 
 window.onload = () =>{
     displayImg();
+    displayCount();
 }
